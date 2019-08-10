b2e62c33b40e8ab88312fd04566706b2bf45b49c
HTMLhead <mrdoo309@gmail.com>
2019-08-11 00:26:41 +0900

    feat: 디버깅 코드 추가

--- route/dataRoute.js
+++ route/dataRoute.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 router.post("/", function(req, res, next) {
   const callSug = require("../public/callSuggestion.js");
+  console.log(req.baseUrl);
   let sugData = req.baseUrl.slice(1, req.baseUrl.length);
-
+  console.log(sugData);
   res.send(callSug(sugData));
 });
 
